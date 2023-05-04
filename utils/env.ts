469f36f39b7c26004eb0e3005bdebefbb0d560c5
shitikyan <shitikyan@gmail.com>
2023-05-04 19:53:05 +0400

    Updated env ts

--- utils/env.ts
+++ utils/env.ts
@@ -1,6 +1,6 @@
 export interface EnvVariables {
-  supabaseUrl?: string;
-  supabaseServiceKey?: string;
+  supabaseUrl: string;
+  supabaseServiceKey: string;
   emailJsServiceId: string;
   emailJsTemplateId: string;
   emailJsPublicKey: string;
@@ -9,12 +9,9 @@ export interface EnvVariables {
 const env = {
   supabaseUrl: process.env.NEXT_PUBLIC_SUPABASE_URL,
   supabaseServiceKey: process.env.NEXT_PUBLIC_SUPABASE_SERVICE_KEY,
-  emailJsServiceId:
-    process.env.NEXT_PUBLIC_EMAIL_SERVICE_ID ?? "service_ex4bvp2",
-  emailJsTemplateId:
-    process.env.NEXT_PUBLIC_EMAIL_TEMPLATE_ID ?? "template_ifnxq6m",
-  emailJsPublicKey:
-    process.env.NEXT_PUBLIC_EMAIL_PUBLIC_KEY ?? "DJCJL9uIp5kDCJb7H",
+  emailJsServiceId: process.env.NEXT_PUBLIC_EMAIL_SERVICE_ID,
+  emailJsTemplateId: process.env.NEXT_PUBLIC_EMAIL_TEMPLATE_ID,
+  emailJsPublicKey: process.env.NEXT_PUBLIC_EMAIL_PUBLIC_KEY,
 };
 
 export default env;
